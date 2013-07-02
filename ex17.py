9de4ebea498193b1f0256019039a16806f4caf3a
Sophie C <sophie.clayton@gmail.com>
2013-07-01 20:46:50 -0400

    even shorter version of exercise 17

--- ex17.py
+++ ex17.py
@@ -2,11 +2,8 @@ from sys import argv
 
 script, from_file, to_file = argv
 
-in_file = open(from_file,'r')
-out_file = open(to_file,'w')
-out_file.write(open(from_file).read())
+open(to_file,'w').write(open(from_file,'r').read())
 
-out_file.close()
-in_file.close()
+
 
 
